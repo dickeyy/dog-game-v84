63e6737ffa5fd5f0f14c20b00b652bb74b55f623
Kyle Dickey <41174949+dickeyy@users.noreply.github.com>
2023-12-12 14:22:13 -0700

    posthog

---
+++ src/lib/posthog.ts
@@ -0,0 +1,5 @@
+import posthog from "posthog-js";
+
+posthog.init("phc_MJXtujy3sNu58uQ2dki1D2YQ2cRlzlZYVImkybhkjHt", { api_host: "https://app.posthog.com" });
+
+export default posthog;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,8 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import posthog from "../lib/posthog";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+    if (posthog) posthog;
+    return <Component {...pageProps} />;
 }
